b7ea2b4122b8b58b46afc0ed161a0ee652c2a4f9
Billy Tseng <tseng.billy@gmail.com>
2017-09-12 20:20:26 -0700

    Revise for pep8

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -5,6 +7,7 @@ from sqlalchemy import create_engine
 import datetime
 
 Base = declarative_base()
+
 
 class Category(Base):
     """Category builds a category table with two column and
@@ -17,11 +20,12 @@ class Category(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class Item(Base):
     """Item builds a item table that has realationship with the category table
@@ -29,27 +33,22 @@ class Item(Base):
     """
     __tablename__ = 'item'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
     description = Column(String(250))
-    category_id = Column(Integer,ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-       }
-
-
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
 
 engine = create_engine('sqlite:///category_item.db')
-
 
 Base.metadata.create_all(engine)
