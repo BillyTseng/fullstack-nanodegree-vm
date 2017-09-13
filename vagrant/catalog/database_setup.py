73ec95a121af00c68bfd8fdabcfaea4dc8620a6f
Billy Tseng <tseng.billy@gmail.com>
2017-09-12 18:15:33 -0700

    Build the database category_item.db

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,55 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import datetime
+
+Base = declarative_base()
+
+class Category(Base):
+    """Category builds a category table with two column and
+       provides serializeable data.
+    """
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+
+class Item(Base):
+    """Item builds a item table that has realationship with the category table
+       and provides serializeable data.
+    """
+    __tablename__ = 'item'
+
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    description = Column(String(250))
+    category_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+       }
+
+
+
+engine = create_engine('sqlite:///category_item.db')
+
+
+Base.metadata.create_all(engine)
