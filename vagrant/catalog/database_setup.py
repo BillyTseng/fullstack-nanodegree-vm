fbe2d66dab46ff13e734064640f0586c68c20d73
Billy Tseng <tseng.billy@gmail.com>
2017-09-13 22:08:35 -0700

    Setup category_item.db

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -35,7 +35,6 @@ class Item(Base):
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    created_date = Column(DateTime, default=datetime.datetime.utcnow)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
