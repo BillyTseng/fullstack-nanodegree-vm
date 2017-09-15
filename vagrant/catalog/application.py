627def2e13153b13fea4ec36a86949b7a74a8079
Billy Tseng <tseng.billy@gmail.com>
2017-09-14 17:02:22 -0700

    Select a specific category shows all the items available for that category.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -17,13 +17,28 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# Show all categories and latest items
 @app.route('/')
 def showCategories():
+    """Show all categories and latest items"""
     categories = session.query(Category).order_by(asc(Category.name))
     items = session.query(Item).order_by(desc(Item.id))
     return render_template(
                 'categories.html', categories=categories, items=items)
+
+
+@app.route('/catalog/<catalog_name>/items')
+def showItems(catalog_name):
+    """Shows all the items available for this category."""
+    categories = session.query(Category).order_by(asc(Category.name))
+    cur_category = session.query(Category).filter_by(name=catalog_name)
+    for i in cur_category:
+        category_id = i.id
+    items = session.query(Item).filter_by(category_id=category_id)
+    return render_template(
+                'items.html',
+                categories=categories,
+                items=items,
+                catalog_name=catalog_name)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
